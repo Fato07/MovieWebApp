fca3c0360d33bd07dffbd9bcc7da0cdd1c1d81bd
fadosu <fadosu@ttu.ee>
2020-03-07 00:29:31 +0200

    added project Files

---
+++ MvcMovie/WebApp/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class MvcMovieContext:DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+    }
+}
